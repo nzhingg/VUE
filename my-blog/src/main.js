2a94ab6589bc24b58c5e2785856d80613adf0dcf
nzhingg <2258190228@qq.com>
2018-01-28 20:20:19 +0800

    :sparkles: 自定义指令 过滤器

--- my-blog/src/main.js
+++ my-blog/src/main.js
@@ -11,8 +11,29 @@ Vue.use(VueResource)
 // 自定义指令
 Vue.directive('rainbow',{
   bind(el,binding,vnode){
-    el.style.color=red;
+    // el.style.color = '#' + Math.random().toString(16).slice(2,8)
   }
+})
+Vue.directive('theme',{
+  bind(el,binding,vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = '1260px';
+    } else if(binding.value == 'narrow'){
+      el.style.maxWidth = '500px';
+    }
+
+    if(binding.arg == 'column'){
+      el.style.background = 'pink';
+      el.style.padding = '20px';
+    }
+  }
+})
+
+// Vue.filter("to-uppercase",function(value){
+//   return value.toUpperCase();
+// })
+Vue.filter("snippet",function(value){
+  return value.slice(0,100)+"...";
 })
 
 /* eslint-disable no-new */
