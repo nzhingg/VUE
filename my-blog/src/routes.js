e77a7a00ff6610b33941521c764f0c8a42e511df
nzhingg <2258190228@qq.com>
2018-01-28 21:09:08 +0800

    :sparkles: 路由配置

---
+++ my-blog/src/routes.js
@@ -0,0 +1,8 @@
+import ShowBlogs from './components/ShowBlogs.vue'
+import AddBlog from './components/AddBlog.vue'
+
+
+export default[
+  {path:'/',component:ShowBlogs},
+  {path:'/add',component:AddBlog}
+]
