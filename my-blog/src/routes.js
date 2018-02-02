0a6d0e0c3e44d01155278b2920687c2d92ebbd97
nzhingg <2258190228@qq.com>
2018-02-02 14:13:48 +0800

    路由参数

--- my-blog/src/routes.js
+++ my-blog/src/routes.js
@@ -1,8 +1,11 @@
 import ShowBlogs from './components/ShowBlogs.vue'
 import AddBlog from './components/AddBlog.vue'
+import SingleBlog from './components/SingleBlog.vue'
 
 
 export default[
   {path:'/',component:ShowBlogs},
-  {path:'/add',component:AddBlog}
+  {path:'/add',component:AddBlog},
+  {path:'/blog/:id',component:SingleBlog}
+  
 ]
